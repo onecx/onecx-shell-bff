f9df4544cd82039615feac914eb0191d8a1f149b
Jorden_Reuter <jorden.reuter@capgemini.com>
2024-03-26 15:51:00 +0100

    fix: formatting

--- src/test/java/org/tkit/onecx/shell/bff/rs/AbstractTest.java
+++ src/test/java/org/tkit/onecx/shell/bff/rs/AbstractTest.java
@@ -1,12 +1,6 @@
 package org.tkit.onecx.shell.bff.rs;
 
-import java.security.PrivateKey;
-
-import jakarta.json.Json;
-import jakarta.json.JsonObjectBuilder;
-
 import org.eclipse.microprofile.config.ConfigProvider;
-import org.eclipse.microprofile.jwt.Claims;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -18,8 +12,6 @@ import io.quarkus.test.keycloak.client.KeycloakTestClient;
 import io.restassured.RestAssured;
 import io.restassured.config.ObjectMapperConfig;
 import io.restassured.config.RestAssuredConfig;
-import io.smallrye.jwt.build.Jwt;
-import io.smallrye.jwt.util.KeyUtils;
 
 @QuarkusTestResource(MockServerTestResource.class)
 public abstract class AbstractTest {
@@ -46,29 +38,4 @@ public abstract class AbstractTest {
                         }));
     }
 
-  /**  protected static String createToken(String userId, String orgId) {
-        try {
-            String userName = userId != null ? userId : "test-user";
-            String organizationId = orgId != null ? orgId : "org1";
-            JsonObjectBuilder claims = createClaims(userName, organizationId);
-
-            PrivateKey privateKey = KeyUtils.generateKeyPair(2048).getPrivate();
-            return Jwt.claims(claims.build()).sign(privateKey);
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-    }
-
-    protected static JsonObjectBuilder createClaims(String userName, String orgId) {
-        JsonObjectBuilder claims = Json.createObjectBuilder();
-        claims.add(Claims.preferred_username.name(), userName);
-        claims.add(Claims.sub.name(), userName);
-        claims.add(CLAIMS_ORG_ID, orgId);
-        claims.add("name", "Given Family " + userName);
-        claims.add(Claims.given_name.name(), "Given " + userName);
-        claims.add(Claims.family_name.name(), "Family " + userName);
-        claims.add(Claims.email.name(), userName + "@cap.de");
-
-        return claims;
-    } */
 }
--- src/test/java/org/tkit/onecx/shell/bff/rs/UserProfileControllerTest.java
+++ src/test/java/org/tkit/onecx/shell/bff/rs/UserProfileControllerTest.java
@@ -1,12 +1,13 @@
 package org.tkit.onecx.shell.bff.rs;
 
-import gen.org.tkit.onecx.shell.bff.rs.internal.model.GetUserProfileResponseDTO;
-import gen.org.tkit.onecx.user.profile.client.model.MenuMode;
-import gen.org.tkit.onecx.user.profile.client.model.UserPerson;
-import gen.org.tkit.onecx.user.profile.client.model.UserProfile;
-import gen.org.tkit.onecx.user.profile.client.model.UserProfileAccountSettings;
+import static io.restassured.RestAssured.given;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
 import jakarta.ws.rs.HttpMethod;
 import jakarta.ws.rs.core.Response;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockserver.client.MockServerClient;
@@ -14,14 +15,14 @@ import org.mockserver.model.JsonBody;
 import org.mockserver.model.MediaType;
 import org.tkit.onecx.shell.bff.rs.controllers.UserProfileRestController;
 
+import gen.org.tkit.onecx.shell.bff.rs.internal.model.GetUserProfileResponseDTO;
+import gen.org.tkit.onecx.user.profile.client.model.MenuMode;
+import gen.org.tkit.onecx.user.profile.client.model.UserPerson;
+import gen.org.tkit.onecx.user.profile.client.model.UserProfile;
+import gen.org.tkit.onecx.user.profile.client.model.UserProfileAccountSettings;
 import io.quarkiverse.mockserver.test.InjectMockServerClient;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
-
-import static io.restassured.RestAssured.given;
-import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
-import static org.mockserver.model.HttpRequest.request;
-import static org.mockserver.model.HttpResponse.response;
 
 @QuarkusTest
 @TestHTTPEndpoint(UserProfileRestController.class)
@@ -30,31 +31,31 @@ public class UserProfileControllerTest extends AbstractTest {
     @InjectMockServerClient
     MockServerClient mockServerClient;
 
-        @Test
-        void getUserProfileTest() {
+    @Test
+    void getUserProfileTest() {
 
-            UserProfile profileResponse = new UserProfile();
-            profileResponse.organization("org1").id("profile1")
-                    .person(new UserPerson().firstName("Max").lastName("Mustermann").email("testEmail"))
-                    .accountSettings(new UserProfileAccountSettings().menuMode(MenuMode.HORIZONTAL));
+        UserProfile profileResponse = new UserProfile();
+        profileResponse.organization("org1").id("profile1")
+                .person(new UserPerson().firstName("Max").lastName("Mustermann").email("testEmail"))
+                .accountSettings(new UserProfileAccountSettings().menuMode(MenuMode.HORIZONTAL));
 
-            // create mock rest endpoint for user-profile-svc
-            mockServerClient.when(request().withPath("/v1/userProfile/me").withMethod(HttpMethod.GET))
-                    .withId("mock")
-                    .respond(httpRequest -> response().withStatusCode(Response.Status.OK.getStatusCode())
-                            .withContentType(MediaType.APPLICATION_JSON)
-                            .withBody(JsonBody.json(profileResponse)));
+        // create mock rest endpoint for user-profile-svc
+        mockServerClient.when(request().withPath("/v1/userProfile/me").withMethod(HttpMethod.GET))
+                .withId("mock")
+                .respond(httpRequest -> response().withStatusCode(Response.Status.OK.getStatusCode())
+                        .withContentType(MediaType.APPLICATION_JSON)
+                        .withBody(JsonBody.json(profileResponse)));
 
-            var output = given()
-                    .when()
-                    .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
-                    .header(APM_HEADER_PARAM, ADMIN)
-                    .get()
-                    .then()
-                    .statusCode(Response.Status.OK.getStatusCode())
-                    .contentType(APPLICATION_JSON)
-                    .extract().as(GetUserProfileResponseDTO.class);
+        var output = given()
+                .when()
+                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
+                .header(APM_HEADER_PARAM, ADMIN)
+                .get()
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .contentType(APPLICATION_JSON)
+                .extract().as(GetUserProfileResponseDTO.class);
 
-            Assertions.assertNotNull(output);
-        }
+        Assertions.assertNotNull(output);
+    }
 }

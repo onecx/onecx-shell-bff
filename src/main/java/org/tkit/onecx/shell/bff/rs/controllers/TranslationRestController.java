9e312a15734666d87082d688051b19c23f52ee29
Maciej Kryger <50527026+maciejkryger@users.noreply.github.com>
2026-03-10 15:45:46 +0100

    feat: dynamic translations from file-storage-svc (#165)
    * feat: dynamic translations from file-storage-svc
    
    * feat: dynamic translations from file-storage-svc - MR remarks
    
    * feat: dynamic translations from file-storage-svc - helm scope added
    
    * feat: dynamic translations from file-storage-svc - MR remarks part 2
    
    * Revert "feat: dynamic translations from file-storage-svc - MR remarks part 2"
    
    This reverts commit c82a51e80bdc2388c8d21b4f9b498b88e2fe9fa6.
    
    * feat: dynamic translations from file-storage-svc - MR remarks part 2.1

--- src/main/java/org/tkit/onecx/shell/bff/rs/controllers/TranslationRestController.java
+++ src/main/java/org/tkit/onecx/shell/bff/rs/controllers/TranslationRestController.java
@@ -5,11 +5,16 @@ import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.core.Response;
 
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.tkit.onecx.shell.bff.rs.mappers.TranslationMapper;
 import org.tkit.quarkus.log.cdi.LogService;
 
+import gen.org.tkit.onecx.file.storage.client.api.FileStorageApi;
+import gen.org.tkit.onecx.file.storage.client.model.PresignedUrlResponse;
 import gen.org.tkit.onecx.shell.bff.rs.internal.TranslationApiService;
 import gen.org.tkit.onecx.shell.bff.rs.internal.model.GetTranslationsRequestDTO;
+import gen.org.tkit.onecx.shell.bff.rs.internal.model.GetTranslationsResponseDTO;
+import gen.org.tkit.onecx.shell.bff.rs.internal.model.TranslationResultDTO;
 
 @ApplicationScoped
 @Transactional(value = Transactional.TxType.NOT_SUPPORTED)
@@ -17,11 +22,24 @@ import gen.org.tkit.onecx.shell.bff.rs.internal.model.GetTranslationsRequestDTO;
 public class TranslationRestController implements TranslationApiService {
 
     @Inject
+    @RestClient
+    FileStorageApi fileStorageClient;
+
+    @Inject
     TranslationMapper mapper;
 
-    // To be implemented
     @Override
     public Response getTranslations(GetTranslationsRequestDTO getTranslationsRequestDTO) {
-        return Response.status(Response.Status.OK).entity(mapper.map(getTranslationsRequestDTO)).build();
+
+        GetTranslationsResponseDTO getTranslationsResponseDTO = new GetTranslationsResponseDTO();
+        getTranslationsRequestDTO.getContext().forEach(translationsContextDTO -> {
+            try (Response response = fileStorageClient
+                    .getPresignedDownloadUrl(mapper.map(translationsContextDTO, getTranslationsRequestDTO))) {
+                var data = response.readEntity(PresignedUrlResponse.class);
+                TranslationResultDTO translationResultDTO = mapper.map(data, translationsContextDTO);
+                getTranslationsResponseDTO.addTranslationsItem(translationResultDTO);
+            }
+        });
+        return Response.status(Response.Status.OK).entity(getTranslationsResponseDTO).build();
     }
 }
--- src/test/java/org/tkit/onecx/shell/bff/rs/TranslationRestControllerTest.java
+++ src/test/java/org/tkit/onecx/shell/bff/rs/TranslationRestControllerTest.java
@@ -2,6 +2,7 @@ package org.tkit.onecx.shell.bff.rs;
 
 import static io.restassured.RestAssured.given;
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.mockserver.model.HttpRequest.request;
 
 import java.util.List;
 
@@ -10,8 +11,13 @@ import jakarta.ws.rs.core.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockserver.client.MockServerClient;
+import org.mockserver.model.HttpResponse;
+import org.mockserver.model.JsonBody;
+import org.mockserver.model.MediaType;
 import org.tkit.onecx.shell.bff.rs.controllers.TranslationRestController;
 
+import gen.org.tkit.onecx.file.storage.client.model.PresignedUrlRequest;
+import gen.org.tkit.onecx.file.storage.client.model.PresignedUrlResponse;
 import gen.org.tkit.onecx.shell.bff.rs.internal.model.*;
 import io.quarkiverse.mockserver.test.InjectMockServerClient;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
@@ -20,18 +26,40 @@ import io.quarkus.test.junit.QuarkusTest;
 @QuarkusTest
 @TestHTTPEndpoint(TranslationRestController.class)
 class TranslationRestControllerTest extends AbstractTest {
+
     @InjectMockServerClient
     MockServerClient mockServerClient;
 
     @Test
     void getTranslationsTest() {
-
+        String token = keycloakClient.getAccessToken(ADMIN);
         GetTranslationsRequestDTO requestDTO = new GetTranslationsRequestDTO();
         requestDTO.setLocale("DE");
         requestDTO.setContext(List.of(new TranslationsContextDTO().artifact("angular-accelerator").version("^8.0.0")));
+
+        PresignedUrlRequest presignedUrlRequest = new PresignedUrlRequest();
+        presignedUrlRequest.setApplicationId("onecx-shell-bff");
+        presignedUrlRequest.setProductName("onecx-shell");
+        presignedUrlRequest.setFileName("DE/angular-accelerator-^8.0.0.json");
+
+        PresignedUrlResponse presignedUrlResponse = new PresignedUrlResponse();
+        presignedUrlResponse.setUrl("https://mocked-url/download");
+
+        mockServerClient
+                .when(request()
+                        .withMethod("POST")
+                        .withPath("/v1/file-storage/presigned/download")
+                        .withHeader(APM_HEADER_PARAM, ADMIN)
+                        .withBody(JsonBody.json(presignedUrlRequest)))
+                .withId("mockPresignedUrl")
+                .respond(HttpResponse.response()
+                        .withStatusCode(200)
+                        .withContentType(MediaType.APPLICATION_JSON)
+                        .withBody(JsonBody.json(presignedUrlResponse)));
+
         var output = given()
                 .when()
-                .auth().oauth2(keycloakClient.getAccessToken(ADMIN))
+                .auth().oauth2(token)
                 .header(APM_HEADER_PARAM, ADMIN)
                 .contentType(APPLICATION_JSON)
                 .body(requestDTO)
@@ -44,5 +72,54 @@ class TranslationRestControllerTest extends AbstractTest {
         Assertions.assertEquals(output.getTranslations().size(), requestDTO.getContext().size());
         Assertions.assertEquals(output.getTranslations().get(0).getArtifact(), requestDTO.getContext().get(0).getArtifact());
         Assertions.assertEquals(output.getTranslations().get(0).getVersion(), requestDTO.getContext().get(0).getVersion());
+
+        mockServerClient.clear("mockPresignedUrl");
+    }
+
+    @Test
+    void getTranslationsNullUrlTest() {
+        String token = keycloakClient.getAccessToken(ADMIN);
+        GetTranslationsRequestDTO requestDTO = new GetTranslationsRequestDTO();
+        requestDTO.setLocale("DE");
+        requestDTO.setContext(List.of(new TranslationsContextDTO().artifact("angular-accelerator").version("^8.0.0")));
+
+        PresignedUrlRequest presignedUrlRequest = new PresignedUrlRequest();
+        presignedUrlRequest.setApplicationId("onecx-shell-bff");
+        presignedUrlRequest.setProductName("onecx-shell");
+        presignedUrlRequest.setFileName("DE/angular-accelerator-^8.0.0.json");
+
+        PresignedUrlResponse presignedUrlResponse = new PresignedUrlResponse();
+        presignedUrlResponse.setUrl(null);
+
+        mockServerClient
+                .when(request()
+                        .withMethod("POST")
+                        .withPath("/v1/file-storage/presigned/download")
+                        .withHeader(APM_HEADER_PARAM, ADMIN)
+                        .withBody(JsonBody.json(presignedUrlRequest)))
+                .withId("mockPresignedUrlNull")
+                .respond(HttpResponse.response()
+                        .withStatusCode(200)
+                        .withContentType(MediaType.APPLICATION_JSON)
+                        .withBody(JsonBody.json(presignedUrlResponse)));
+
+        var output = given()
+                .when()
+                .auth().oauth2(token)
+                .header(APM_HEADER_PARAM, ADMIN)
+                .contentType(APPLICATION_JSON)
+                .body(requestDTO)
+                .post()
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .contentType(APPLICATION_JSON)
+                .extract().as(GetTranslationsResponseDTO.class);
+        Assertions.assertNotNull(output);
+        Assertions.assertEquals(requestDTO.getContext().size(), output.getTranslations().size());
+        Assertions.assertNull(output.getTranslations().get(0).getUrl());
+        Assertions.assertEquals(requestDTO.getContext().get(0).getArtifact(), output.getTranslations().get(0).getArtifact());
+        Assertions.assertEquals(requestDTO.getContext().get(0).getVersion(), output.getTranslations().get(0).getVersion());
+
+        mockServerClient.clear("mockPresignedUrlNull");
     }
 }
